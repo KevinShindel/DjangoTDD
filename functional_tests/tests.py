7041afeb80dc3ea8265c67741bae47e9fbfdbda1
KevinShindel <kevin.shindel@yahoo.com>
2022-02-10 09:26:06 +0200

    multi users test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -17,9 +17,9 @@ class NewVisitorTest(LiveServerTestCase):
     MAX_WAIT = 3
 
     def setUp(self) -> None:
-        service = Service(executable_path=GECKO_DRIVER, log_path=join(dirname(GECKO_DRIVER), 'log.txt'))
-        options = Options()
-        self.browser = webdriver.Firefox(service=service, options=options)
+        self.service = Service(executable_path=GECKO_DRIVER, log_path=join(dirname(GECKO_DRIVER), 'log.txt'))
+        self.options = Options()
+        self.browser = webdriver.Firefox(service=self.service, options=self.options)
 
     def wait_for_row_in_lista_table(self, row_text: str) -> None:
         start_time = time.time()
@@ -33,6 +33,37 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > self.MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
+    def test_can_start_a_list_for_one_user(self):
+        pass
+
+    def test_multi_user_can_start_a_list_at_diff_urls(self):
+        ''' многочисленные пользователи могут начать списки по разным url '''
+        self.browser.get(self.live_server_url)
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        input_box.send_keys('Купить павлиньи перья')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_lista_table('1: Купить павлиньи перья')
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox(self.service, self.options)
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать мушку', page_text)
+
+        input_box = self.browser.find_element(By.ID, 'id_new_item')
+        input_box.send_keys('Купить молоко')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_lista_table('1: Купить молоко')
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Эдит слышала про крутое онлайн приложение неотложных дел.
