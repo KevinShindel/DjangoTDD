3bc2db5c1b72e616d755b2a1dde2eea7f36803ac
KevinShindel <kevin.shindel@yahoo.com>
2022-03-07 14:37:42 +0200

    рефакторизация жестко кодированых url

--- lists/urls.py
+++ lists/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from lists.views import new_list, view_list
 
 list_patterns = [
-    path('new', new_list),
+    path('new', new_list, name='new_list'),
     path('<int:list_id>/', view_list, name='view_list'),
 ]
